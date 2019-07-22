62b4596eaaed864b47abcc518a521714e1a31eea
kb_cc <zhaoyx0907@163.com>
2019-07-22 21:34:03 +0800

    init project

---
+++ config-overrides.js
@@ -0,0 +1,13 @@
+const path = require('path')
+
+const { override, addWebpackAlias } = require('customize-cra')
+
+module.exports = override(
+  addWebpackAlias({
+    assets: path.resolve(__dirname, './src/assets'),
+    components: path.resolve(__dirname, './src/components'),
+    utils: path.resolve(__dirname, './src/utils'),
+    store: path.resolve(__dirname, './src/store'),
+    pages: path.resolve(__dirname, './src/pages')
+  })
+)
