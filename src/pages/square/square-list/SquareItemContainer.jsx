b7d8fe79500ead17485d2a9e5848570d5036beea
Pmantou <1402649723@qq.com>
2019-08-08 09:00:51 +0800

    下拉加载更多

--- src/pages/square/square-list/SquareItemContainer.jsx
+++ src/pages/square/square-list/SquareItemContainer.jsx
@@ -7,6 +7,7 @@ import StyleSquareItemContainer from './StyleSquareItemContainer'
 //搜索框组件导入
 import SearchBar from 'components/search/SearchBar.jsx'
 
+
 import http from 'utils/http'
 
 export class SquareItemContainer extends Component {
@@ -14,70 +15,45 @@ export class SquareItemContainer extends Component {
         super(props)
         this.state = {
             picNumber:1,
-            list: []
+            list: [],
         }
+        this.showText = this.showText.bind(this)
+        this.arrowStatus = false
         // this.arrow=React.createRef();
     }
 
     //显示
     showText(textEl,arrowEl){
         let show  = false
+        let that = this
         return function(){
             show = !show
+            that.arrowStatus = !that.arrowStatus
             if(show){
                 textEl.classList.remove('text-container')
                 textEl.style.overFlow = "visible"
-                // let _tem = arrowEl ? arrowEl.style.WebkitTransform = 'rotateX(180deg)' : ''
-                console.log(arrowEl.childNodes)
             }else{
                 textEl.classList.add('text-container')
                 textEl.style.overFlow = "hidden"
-                // let _tem = arrowEl ? arrowEl.style.WebkitTransform = 'rotateX(0deg)' : ''
             }
         }
     }
 
-    async componentDidMount(){
-        let focus = '',
-            time = ''
-        switch(this.props.tabNumber){
-            case 0 :
-                focus = 'focus'
-                time = 'desc'
-                break
-            case 1 :
-                focus = ''
-                time = ''
-                break
-            case 2 :
-                focus = ''
-                time = 'desc'
-                break
+    addLike(pre){
+        return ()=>{
+            console.log(pre)
         }
-        let result = await http.post('/alldynamic',{
-            "focus": focus,
-            "time": time,
-            "pagenum":1,
-            "pagesize":10
-        })
-        this.setState({list: result})
-        // setTimeout(()=>{
-        //     console.log("我是ref",this.arrow)
-        // }, 500)
-
     }
 
     render() {
-        console.log(this.state.list)
-        let list = this.state.list instanceof Array 
-        ? this.state.list.map((item,index)=>(
-                <SquareItemUI key={item.id} showText={this.showText} info={item} tabNumber = {this.props.tabNumber}></SquareItemUI>
+        let list = this.props.squareData
+        ? this.props.squareData.list.map((item,index)=>(
+                <SquareItemUI key={item.id} arrowStatus={this.arrowStatus}  addLike={this.addLike} showText={this.showText} info={item} tabNumber = {this.props.tabNumber}></SquareItemUI>
           )) 
         : []
         return (
         <StyleSquareItemContainer>
-            {this.props.hasSearch ? <SearchBar /> : ''}
-            {/* <SquareItemUI></SquareItemUI> */}
+            {this.props.tabNumber===1 ? <SearchBar /> : ''}
             {list}
         </StyleSquareItemContainer>
         )
