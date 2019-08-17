91ae94a3c85fdf7b1f12cd2b549f092b51b6a8ac
kb_cc <zhaoyx0907@163.com>
2019-08-17 20:28:22 +0800

    修复SquarequareItemUI.jsx -zhaoyx

--- src/pages/square/square-list/SquareItemUI.jsx
+++ src/pages/square/square-list/SquareItemUI.jsx
@@ -2,7 +2,7 @@ import React, { useState, useRef, useEffect } from 'react'
 import StyleSquareItemUI from './StyleSquareItemUI'
 import Interaction from './Interaction/Interaction'
 import Voice from './voice/Voice'
-import { Link } from 'react-router-dom'
+import {Link} from 'react-router-dom'
 // import { CSSTransition } from 'react-transition-group'
 import './TextTransition.css'
 
@@ -12,8 +12,8 @@ import headPhoto from 'assets/images/square/7bdc97e546fb59e65584b916021b193addde
 import arrowDPic from 'assets/images/square/箭头@3x.png'
 
 
-export default (props) => {
-    let [isExceed, setIsExceed] = useState(false)
+export default (props)=>{
+    let [isExceed,setIsExceed] = useState(false)
     const fileInputEl = useRef(null);
     const textEl = useRef(null);
     let elHeight = 0
@@ -21,40 +21,40 @@ export default (props) => {
     let picWidth = 0
     let picHeight = 0
     let picNumber = props.info.image instanceof Array && props.info.image.length
-    useEffect(() => {
+    useEffect(()=>{
         elHeight = fileInputEl.current ? fileInputEl.current.offsetHeight : 0
         elWidth = fileInputEl.current ? fileInputEl.current.offsetWidth : 0
-        if (elHeight > elWidth * (138 / 285)) {
+        if(elHeight > elWidth*(138/285)){
             setIsExceed(true)
-        } else {
+        }else{
             setIsExceed(false)
         }
-
+        
 
     })
-    return (
-        <StyleSquareItemUI>
+    return(
+        <StyleSquareItemUI goods = {props.info.star}>
             <div className="square-user">
                 {/* 头像 */}
-                <Link to={'/mine/friend/' + props.info.uid}>
-                    <img className="user-photo" src={headPhoto} alt="" />
+                <Link to={'/mine/friend/'+props.info.uid}>
+                <img className="user-photo" src={headPhoto} alt=""/>
                 </Link>
                 <div className="user-info">
-                    <span className="username">{props.tabNumber === 1 ? props.info.username : '来自 # ' + props.info.starname}</span>
+                    <span className="username">{ props.tabNumber===1 ? props.info.username : '来自 # '+ props.info.starname}</span>
                     <span className="publish-time">2小时前</span>
                 </div>
             </div>
             <div className="publish-message">
-
-                <div className="publish-pic">
+                
+                <div className="publish-pic"> 
                     {
-                        props.info.libname && <Voice />
+                        props.info.image && !(props.info.image instanceof Array) && <Voice voiceId = {props.info.image} />
                     }
                     {
                         // props.info.image && props.info.image.split(",").map((item,index)=>{
-                        props.info.image instanceof Array && props.info.image.map((item, index) => {
+                        props.info.image instanceof Array && props.info.image.map((item,index)=>{
                             //picNumber=2，2为测试数据
-                            switch (picNumber = 2) {
+                            switch(picNumber=2){
                                 case 1:
                                     picWidth = '2.09rem'
                                     picHeight = '1.17rem'
@@ -72,15 +72,15 @@ export default (props) => {
                                     picHeight = '.76rem'
 
                             }
-                            return (
+                            return(
                                 <>
-                                    <img key={item} src={item} style={{ width: picWidth, height: picHeight, marginRight: index === 1 && picNumber === 2 ? '.90rem' : '.05rem', marginBottom: picNumber === 2 ? '.04rem' : 0 }} alt="" />
-                                    {/* {item} 测试用 */}
+                                    <img key={index} src={item} style={{ display:'block', width:picWidth,height:picHeight,marginRight: index===1&&picNumber===2?'.90rem':'.05rem',marginBottom: picNumber===2 ? '.04rem' :0 }} alt=""/>
+                                    {"测试用"}
                                 </>
                             )
-                        })
+                        }) 
                     }
-
+                    
                 </div>
                 {/* <CSSTransition
                     in={window.flag}
@@ -89,20 +89,14 @@ export default (props) => {
                     onEntered={(el) => {el.style.color='blue'}}
                     appear={true}
                 > */}
-                <h6 className="text-container" ref={textEl}>{props.info.note}春天的味道是蒲公英的味道,是绿色的味道，我妈平时说我笨我家狗就在旁边听着，以至于它真<br />
-                    的觉得我笨出花了<br />
-                    哈哈哈哈<br />
-                    哈哈哈哈<br />
-                    哈哈哈哈<br />
-                    哈哈哈哈<br />
-                    hiehei<br />
-                    hiehei<br />
-                </h6>
+                    <h6 className="text-container" ref={textEl}>
+                        {props.info.note}
+                    </h6>
                 {/* </CSSTransition> */}
                 <h6 className="text-container-measure" ref={fileInputEl}>
                     {props.info.note}
                 </h6>
-                {isExceed && <p onClick={props.showText(textEl.current, props.info.id, props.tabNumber)}><img className={`textArrow${props.info.id}${props.tabNumber}`} src={arrowDPic} alt="" /></p>}
+                {isExceed && <p onClick={props.showText(textEl.current,props.info.id,props.tabNumber)}><img className={`textArrow${props.info.id}${props.tabNumber}`} src={ arrowDPic } alt=""/></p>}
                 {props.info.address && <div className="publish-position">
                     <i className="position-pic"></i><span>{props.info.address}</span>
                 </div>}
