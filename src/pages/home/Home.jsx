7f61b609fa54cb196a5f02bdc3ebcbe784ec7540
zhengkaixuan <zhengkaixuan233@163.com>
2019-08-17 15:00:09 +0800

    改善路由

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
 import { Route, Link, Redirect } from 'react-router-dom'
+
 import PlanetContainer from './planet/PlanetContainer'
 import PlazaContainer from './plaza/PlazaContainer'
+import Publish from 'pages/publish/index.jsx'
 import MineContainer from './mine/MineContainer'
 import ChatContainer from './chat/ChatContainer'
-import PublishContainer from './publish/PublishContainer'
 import StyleHome from './StyleHome'
+
+import ChatRoot from 'pages/chat/ChatRoot'
+// import FocusListContainer from 'pages/chat/focusList/FocusListContainer'
+import { Square } from 'pages/square'
+import { Mine } from 'pages/mine/'
 
 import plazaImg from 'assets/images/home/guangchang3x.png'
 import chatImg from 'assets/images/home/liaotian3x.png'
@@ -25,37 +31,43 @@ import minedImg from 'assets/images/home/mined.png'
 */
 
 export default class Home extends Component {
-  
+
     render() {
         const routePath = this.props.location.pathname
         return (
             <StyleHome>
                 <main>
                     {/* 星球组件 */}
-                    <Redirect from='/' to='/dynamic'></Redirect>
+                    <Redirect from='/home' to='/dynamic' exact></Redirect>
                     <Route path='/home' exact render={ (props)=><PlanetContainer {...props}></PlanetContainer> } />
                     {/* 广场组件 */}
-                    <Route path='/home/dynamic' render={(props)=><PlazaContainer {...props}></PlazaContainer> }/>
+                    <Route path='/dynamic' render={(props)=><PlazaContainer {...props}></PlazaContainer> }/>
                     {/* 发布组件 */}
-                    <Route path='/home/publish' render={(props)=><PublishContainer {...props}></PublishContainer> } />
+                    {/* <Route path='/publish' render={(props)=><Publish {...props}></Publish> } /> */}
                     {/* 聊天组件 */}
-                    <Route path='/home/chat' render={(props)=><ChatContainer {...props}></ChatContainer> } />
+                    <Route path='/chat' render={(props)=><ChatContainer {...props}></ChatContainer> } />
                     {/* 我的组件 */}
-                    <Route path='/home/mine' render={(props)=><MineContainer {...props}></MineContainer> } />
+                    <Route path='/mine' render={(props)=><MineContainer {...props}></MineContainer> }/>
+
+                    {/* <Route path='/home' component={PlanetContainer} />
+                    <Route path='/chat' component={ChatRoot}></Route> */}
+                    {/* <Route path='/publish' component={Publish} /> */}
+                    {/* <Route path='/focus' component={FocusListContainer}></Route> */}
+                    {/* <Route path='/dynamic' component={Square}></Route>
+                    <Route path='/mine' component={Mine}></Route> */}
                 </main>
 
                 <nav>
                     <ul>
                         <li>
                             <Link className="link" to="/home">
-                                <img src={routePath==='/home' ? planetedImg : planetImg} alt="" className="planet-img"/>
+                                <img src={routePath === '/home' ? planetedImg : planetImg} alt="" className="planet-img" />
                                 <span>星球</span>
                             </Link>
                         </li>
                         <li>
-                            <Link className="link" to="/home/dynamic">
-                                {console.log(routePath)}
-                                <img src={routePath==='/home/dynamic' ? plazaedImg : plazaImg} alt="" />
+                            <Link className="link" to="/dynamic">
+                                <img src={routePath === '/dynamic' ? plazaedImg : plazaImg} alt="" />
                                 <span>广场</span>
                             </Link>
                         </li>
@@ -68,14 +80,14 @@ export default class Home extends Component {
                             </Link>
                         </li>
                         <li>
-                            <Link className="link" to="/home/chat">
-                                <img src={routePath==='/home/chat' ? chatedImg : chatImg} alt="" />
+                            <Link className="link" to="/chat">
+                                <img src={routePath === '/chat' ? chatedImg : chatImg} alt="" />
                                 <span>聊天</span>
                             </Link>
                         </li>
                         <li>
-                            <Link className="link" to="/home/mine">
-                                <img src={routePath==='/home/mine' ? minedImg : mineImg} alt="" />
+                            <Link className="link" to="/mine">
+                                <img src={routePath === '/mine' ? minedImg : mineImg} alt="" />
                                 <span>自己</span>
                             </Link>
                         </li>
