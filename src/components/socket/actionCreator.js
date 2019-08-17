ec8b4178ef776e78ceef319cceedba2cc163cf91
haojieyu2006@163.com <haojieyu2006@163.com>
2019-08-17 15:53:00 +0800

    聊天

--- src/components/socket/actionCreator.js
+++ src/components/socket/actionCreator.js
@@ -1,7 +1,9 @@
+var test = true
+var currentId = "1004"
 const createSocket = (from) => {
     return (dispatch) => {
         if ("WebSocket" in window) {
-            let ws = new WebSocket("ws://localhost:8081/" + from)
+            let ws = new WebSocket("ws://139.199.34.125:7007/chat/" + from)
             ws.onopen = function () {
                 // setMessage("连接成功")
                 console.log('socket链接成功')
@@ -11,24 +13,24 @@ const createSocket = (from) => {
                 })
             }
             ws.onmessage = function (event) {
-                // setMessage(event.data)
-                console.log(event.data)
-                console.log(JSON.parse(event.data), "接收到消息----------------")
+                console.log(event, "接收到消息----------------")
+                // console.log(JSON.parse(event.data), "接收到消息----------------")
+                let to = event.currentTarget.url.substring(event.currentTarget.url.length - 4)
                 if (event.data != '对方不在线') {
-                    let _data=JSON.parse(event.data)
+                    let _data = test ? null : JSON.parse(event.data)
                     dispatch({
                         type: 'pushMsg',
-                        payload: { 
-                            fromId: _data.from, 
-                            to:_data.to,
-                            msg: _data.message, 
-                            timeStamp: event.timeStamp 
+                        payload: {
+                            fromId: test ? (to == currentId ? "1001" : "1004") : _data.from,
+                            to: test ? (to == currentId ? "1004" : "1001") : _data.to,
+                            msg: test ? event.data : _data.message,
+                            timeStamp: event.timeStamp
                         }
                     })
 
                 }
             }
-// ws://139.199.34.125:7007/chat/
+            // ws://localhost:8081/
             ws.onerror = function () {
                 // setMessage("出错了")
                 console.log('出错了')
--- src/components/socket/sendMsg.js
+++ src/components/socket/sendMsg.js
@@ -1,13 +1,20 @@
+var test=true;
 function sendMsg(ws = null, {
     to = '',
     from = '',
     msg = '没写消息'
 }) {
     if (ws) {
-        // var msg = '{"from":"' + from + '","to":"' + to + '","message":"' + msg + '"}'
-        let message = { from, to, msg }
-        ws.send(JSON.stringify(message))
-        console.log("send============,",message)
+        if(test){
+            var msg = '{"from":"' + from + '","to":"' + to + '","message":"' + msg + '"}'
+            ws.send(msg)
+            console.log("send============,",msg)
+        }else{
+            let message = { from, to, msg }
+            ws.send(JSON.stringify(message))
+
+            console.log("send============,",message)
+        }
         return true
     } else {
         return false
